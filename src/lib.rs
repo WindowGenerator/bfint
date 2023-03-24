d84361c75305720d8b2a4eff268acf92d80ccc1a
Sergey Chudov <sergey.chudov@bostongene.com>
2023-03-25 00:17:00 +0400

    Fix some stuff, and added roadmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,39 @@
 use std::io::{self, Read, Write};
 
 
+enum Instruction {
+    IncrementPointer,
+    DecrementPointer,
+    IncrementValue,
+    DecrementValue,
+    WriteOperation,
+    ReadOperation,
+    Loop(Vec<Instruction>)
+}
+
 #[derive(Clone)]
 enum Lexem {
     IncrementPointer,
     DecrementPointer,
     IncrementValue,
     DecrementValue,
-    Write,
-    Read,
-    LoopBegin,
-    LoopEnd,
-}
-
-
-enum Instruction {
-    IncrementPointer,
-    DecrementPointer,
-    IncrementValue,
-    DecrementValue,
-    Write,
-    Read,
-    Loop(Vec<Instruction>)
+    WriteOperation,
+    ReadOperation,
+    LoopBegining,
+    LoopEnding,
 }
 
 
 const MEMORY_SIZE: usize = 30000;
 
-fn get_lexems(code: Vec<u8>) -> Vec<Lexem> {
-    code.iter()
-        .filter_map(|&b| match b {
-            b'.' => Some(Lexem::Write),
-            b',' => Some(Lexem::Read),
-            b'+' => Some(Lexem::IncrementValue),
-            b'-' => Some(Lexem::DecrementValue),
-            b'>' => Some(Lexem::IncrementPointer),
-            b'<' => Some(Lexem::DecrementPointer),
-            b'[' => Some(Lexem::LoopBegin),
-            b']' => Some(Lexem::LoopEnd),
-            _ => None
-        })
-        .collect()
-}
-
-fn parse_into_instructions(lexems: &[Lexem]) -> Vec<Instruction> {
-    let mut instructions: Vec<Instruction> = Vec::new();
-    let mut loop_stack: i32 = 0;
-    let mut loop_start: usize = 0;
-
-    for (i, op) in lexems.iter().enumerate() {
-        if loop_stack == 0 {
-            let instruction = match op {
-                Lexem::Write => Some(Instruction::Write),
-                Lexem::Read => Some(Instruction::Read),
-                Lexem::IncrementValue => Some(Instruction::IncrementValue),
-                Lexem::DecrementValue => Some(Instruction::DecrementValue),
-                Lexem::IncrementPointer => Some(Instruction::IncrementPointer),
-                Lexem::DecrementPointer => Some(Instruction::DecrementPointer),
-                Lexem::LoopBegin => {
-                    loop_start = i;
-                    loop_stack += 1;
-                    None
-                },
-                Lexem::LoopEnd => panic!("loop ending at '{}' has no beginning", i),
-            };
-
-            match instruction {
-                Some(instruction) => instructions.push(instruction),
-                None => ()
-            }
-        } else {
-            match op {
-                Lexem::LoopBegin => {
-                    loop_stack += 1;
-                },
-                Lexem::LoopEnd => {
-                    loop_stack -= 1;
-
-                    if loop_stack == 0 {
-                        instructions.push(Instruction::Loop(parse_into_instructions(&lexems[loop_start+1..i])));
-                    }
-                },
-                _ => (),
-            }
-        }
-    }
-
-    if loop_stack != 0 {
-        panic!("a loop that starts at '{}' doesn't have a matching ending!", loop_start);
-    }
-
-    instructions
-}
 
 fn interpret_instructions(instructions: &[Instruction], memory: &mut [u8], data_ptr: &mut u8) {
     for instruction in instructions {
         match instruction {
-            Instruction::Write => {
+            Instruction::WriteOperation => {
                 let _ = io::stdout().write_all(&[memory[*data_ptr as usize]]);
             },
-            Instruction::Read => {
+            Instruction::ReadOperation => {
                 let mut input = [0u8];
                 io::stdin().read_exact(&mut input).expect("failed to read stdin");
                 memory[*data_ptr as usize] = input[0];
@@ -115,6 +49,74 @@ fn interpret_instructions(instructions: &[Instruction], memory: &mut [u8], data_
             }
         }
     }
+}
+
+
+fn parse_into_instructions(lexems: &[Lexem]) -> Vec<Instruction> {
+    let mut instructions: Vec<Instruction> = Vec::new();
+
+    let mut loop_stack_ptr: i32 = 0;
+    let mut loop_start_ptr: usize = 0;
+
+    for (cur_ptr, operation) in lexems.iter().enumerate() {
+        if loop_stack_ptr == 0 {
+            let instruction = match operation {
+                Lexem::WriteOperation => Some(Instruction::WriteOperation),
+                Lexem::ReadOperation => Some(Instruction::ReadOperation),
+                Lexem::IncrementValue => Some(Instruction::IncrementValue),
+                Lexem::DecrementValue => Some(Instruction::DecrementValue),
+                Lexem::IncrementPointer => Some(Instruction::IncrementPointer),
+                Lexem::DecrementPointer => Some(Instruction::DecrementPointer),
+                Lexem::LoopBegining => {
+                    loop_start_ptr = cur_ptr;
+                    loop_stack_ptr += 1;
+                    None
+                },
+                Lexem::LoopEnding => panic!("loop ending at '{}' has no beginning", cur_ptr),
+            };
+
+            match instruction {
+                Some(instruction) => instructions.push(instruction),
+                None => ()
+            }
+        } else {
+            match operation {
+                Lexem::LoopBegining => {
+                    loop_stack_ptr += 1;
+                },
+                Lexem::LoopEnding => {
+                    loop_stack_ptr -= 1;
+
+                    if loop_stack_ptr == 0 {
+                        instructions.push(Instruction::Loop(parse_into_instructions(&lexems[loop_start_ptr + 1 ..cur_ptr])));
+                    }
+                },
+                _ => (),
+            }
+        }
+    }
+
+    if loop_stack_ptr != 0 {
+        panic!("a loop that starts at '{}' doesn't have a matching ending!", loop_start_ptr);
+    }
+
+    instructions
+}
+
+fn get_lexems(code: Vec<u8>) -> Vec<Lexem> {
+    code.iter()
+        .filter_map(|&b| match b {
+            b'.' => Some(Lexem::WriteOperation),
+            b',' => Some(Lexem::ReadOperation),
+            b'+' => Some(Lexem::IncrementValue),
+            b'-' => Some(Lexem::DecrementValue),
+            b'>' => Some(Lexem::IncrementPointer),
+            b'<' => Some(Lexem::DecrementPointer),
+            b'[' => Some(Lexem::LoopBegining),
+            b']' => Some(Lexem::LoopEnding),
+            _ => None
+        })
+        .collect()
 }
 
 pub fn interpret(code: Vec<u8>) {
