a3252643a6402de3486096a4051c466d631ab2f3
Window Generator <chudov42@gmail.com>
2023-03-30 23:08:13 +0400

    [FEATURE] cli: Added CLI implementation

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 # bfint
 
-BrainFuck interpreter implemented in Rust.
+This is a BrainFuck interpreter implemented in Rust. BrainFuck is an esoteric programming language known for its minimalism and extreme simplicity.
 
+## Usage
+
+The bfint-cli run command supports two options:
+
+- `[FILE]`: Specifies the file containing the BrainFuck code to run.
+- `-c --code <CODE>`: Positional an argument with BrainFuck code.
+
+Running BrainFuck code with positional argument:
+```bash
+bfint-cli -c "--<-<<+[+[<+>--->->->-<<<]>]<<--.<++++++.<<-..<<.<+.>>.>>.<<<.+++.>>.>>-.<<<+."
+```
+
+Running BrainFuck code from a file:
+```bash
+bfint-cli ./examples/hello_world.bf
+```
 
 ## Roadmap
-
 - [x] Write interpreter core
-- [ ] Write tests for interpreter
+- [x] Write tests for interpreter
 - [ ] Add examples
 - [x] Add ci/cd 
-- [ ] Add normal README
-- [ ] Add normal cli to project
+- [x] Add normal README
+- [x] Add normal cli to project
 
+## Contributing
+If you'd like to contribute to this project, feel free to fork the repository and submit a pull request.
+
+Please make sure to write tests for any new functionality you add, and ensure that all existing tests continue to pass.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
 
--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,43 @@
 use bfint::interpret;
-use std::io::{self, Read};
+use std::fs;
+use std::io::{self};
 
-fn get_code_from_stdin() -> io::Result<Vec<u8>> {
-    let mut buf: Vec<u8> = vec![];
-    io::stdin().read_to_end(&mut buf).expect("aboba");
-    Ok(buf)
+use clap::{Args, Parser};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+#[group(required = true, multiple = false)]
+struct Cli {
+    /// Path to file with brainfuck code
+    #[arg()]
+    file: Option<String>,
+    /// Brainfuck source code
+    #[arg(short, long)]
+    code: Option<String>,
 }
 
 fn main() {
-    let code = match get_code_from_stdin() {
-        Ok(code) => code,
-        Err(_) => panic!("failed to parse code"),
-    };
+    let cli = Cli::parse();
 
-    interpret(code, &mut io::stdout(), &mut io::stdin())
+    if let Some(source_code) = &cli.code {
+        interpret(
+            source_code.as_bytes().to_vec(),
+            &mut io::stdout(),
+            &mut io::stdin(),
+        )
+    }
+
+    if let Some(file_path) = &cli.file {
+        match fs::read(file_path) {
+            Ok(source_code) => interpret(source_code, &mut io::stdout(), &mut io::stdin()),
+            Err(err) => {
+                println!(
+                    "Cannot read file with path: '{}', error: '{}'",
+                    file_path, err
+                );
+                ::std::process::exit(1)
+            }
+        }
+    }
 }
